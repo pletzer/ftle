88c82f67498c719707584d8a8fb96aeace06bfc1
Alexander Pletzer <alexander@gokliya.net>
2025-12-21 15:43:39 +1300

    trying to get openmp working in pv

--- paraview_plugin/cmake_command.sh
+++ paraview_plugin/cmake_command.sh
@@ -2,6 +2,10 @@ mkdir build
 cd build
 deactivate # deactivate venv if loaded
 export PYTHON_EXECUTABLE=/Applications/ParaView-6.0.1.app/Contents/bin/pvpython
-cmake ..
+cmake \
+  -DCMAKE_C_COMPILER=/opt/homebrew/opt/llvm/bin/clang \
+  -DCMAKE_CXX_COMPILER=/opt/homebrew/opt/llvm/bin/clang++ \
+  -DCMAKE_BUILD_TYPE=Release \
+  ..
 make -j
 
